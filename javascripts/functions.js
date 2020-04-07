681ac6af774e0f03a9f28f50f615efa09ebeafe7
Morgan Bartlett <mbartlet@uoregon.edu>
2020-04-07 14:57:00 -0700

    Add arrMax to functions.js

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -1,10 +1,20 @@
 //jshint esversion: 6
 
 // Step 3B - average of array function
-let avgArr = arr => {
+let arrAvg = arr => {
   let sum = 0;
   for (var i = 0; i < arr.length; i++) {
     sum = sum + arr[i];
   }
   return sum / arr.length;
+};
+
+// Step 3C - return largest number in array function
+let arr = [];
+let largest = 0;
+let arrMax = arr => {
+  arr.forEach(function(value) {
+    if (largest < value) largest = value;
+  });
+  console.log(largest);
 };
