242fd775007d2aff1dfd222225601b6f2a0ea273
Morgan Bartlett <mbartlet@uoregon.edu>
2020-04-09 16:59:33 -0700

    Added and finalized diceNamic.html, diceNamic.css, and diceNamic.js

--- javascripts/diceNamic.js
+++ javascripts/diceNamic.js
@@ -8,10 +8,46 @@ let rollDie = () => {
 
 //Define a controller to handle the button click
 let controller = function() {
-  //Input: prompt the user for cost and diameter
-  //Processing: assemble the result string
-  //use <br> in the result string for html newline
-  //Output: write the result string into the empty div
+  let target,
+    die1,
+    die2,
+    rollCount = 0,
+    die1URL,
+    die2URL,
+    imgElem1,
+    imgElem2,
+    counterMsg;
+
+  //get the target number from the web page
+  target = document.querySelector("input").value;
+  console.log(target);
+
+  //roll the target number (and count number of rolls)
+  do {
+    //roll the dice
+    die1 = rollDie();
+    die2 = rollDie();
+    //increment the roll counter
+    if (die1 + die2 !== target) {
+      ++rollCount;
+    }
+    //log the dice and the counter
+    console.log(`d1 = ${die1} d2 = ${die2} roll # = ${rollCount}`);
+  } while (die1 + die2 != target);
+  //update images on web page
+  die1URL = `images/die${die1}.gif`;
+  imgElem1 = document.querySelector("#imgElem1");
+  imgElem1.setAttribute("src", die1URL);
+
+  die2URL = `images/die${die2}.gif`;
+  imgElem2 = document.querySelector("#imgElem2");
+  imgElem2.setAttribute("src", die2URL);
+
+  //display number of rolls on webpage
+  counterMsg = `You hit your number in ${rollCount} roll${
+    rollCount == 1 ? "" : "s"
+  }!`;
+  document.querySelector("#outputDiv").innerHTML = counterMsg;
 };
 
 //Register the controller after the DOM is complete
